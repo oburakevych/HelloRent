49fa3e0c12e2c3b48bc9a951b8244c578a58a2cf
oburakevych <oburakevych@gmail.com>
2014-11-26 23:01:05 +1100

    add credit score to the firebase

--- app/js/app.js
+++ app/js/app.js
@@ -647,28 +647,30 @@ App.controller('RegisterFormController', ['$scope', '$http', '$state', function(
 App.controller('ApplicationsController', ['$scope', '$rootScope', '$firebase', "$timeout", function($scope, $rootScope, $firebase, $timeout){
   var ref = new Firebase("hello-rent.firebaseio.com");
   var sync = $firebase(ref.child("applications"));
-
+  
   $rootScope.applications = sync.$asObject();
-
 }]);
 
 
-App.controller('ApplicationController', ['$scope', '$rootScope', '$stateParams', '$firebase', function($scope, $rootScope, $stateParams, $firebase){
+App.controller('ApplicationController', ['$scope', '$rootScope', '$stateParams', '$firebase', function($scope, $rootScope, $stateParams, $firebase) {
   console.log($stateParams);
 
   $scope.application = $rootScope.applications[$stateParams.id];
+  
+  var ref = new Firebase("hello-rent.firebaseio.com");
+  $scope.CREDIT_SCORE = $firebase(ref.child("creditScore")).$asObject();
 
   $scope.getCreditReport = function(score) {
-  	angular.forEach(App.creditReport, function(report, key) {
-  		if (report.min <= score && report.max >= score) {
+  	angular.forEach($scope.CREDIT_SCORE, function(report, key) {
+      if (report.min <= score && report.max >= score) {
   			$scope.report = report;
   		}
   	});
   }
 
-  if ($scope.application) {
-  	$scope.getCreditReport($scope.application.creditScore);
-  }
+  $scope.CREDIT_SCORE.$loaded().then(function() {
+    $scope.getCreditReport($scope.application.creditScore);
+  });
 }]);
 /**=========================================================
  * Module: calendar-ui.js
